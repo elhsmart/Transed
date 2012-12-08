9c29b4754769c2135f64c69a5536c3c1f26d467c
Ed "smart" Tretyakov <elhsmart@gmail.com>
2012-12-08 23:09:27 +0200

    Google Analytics

--- js/editor.js
+++ js/editor.js
@@ -43,30 +43,30 @@ var TransedEditor = {
     popup: {
         about: {
             frame: "none",
-            width: 600,
-            height: 300,
-            minWidth: 600,
-            minHeight: 300,
-            left: 0,
-            top: 0
+            width:      600,
+            height:     300,
+            minWidth:   600,
+            minHeight:  300,
+            left:       0,
+            top:        0
         },
         confirm: {
             frame: "none",
-            width: 600,
-            height: 300,
-            minWidth: 600,
-            minHeight: 300,
-            left: 0,
-            top: 0
+            width:      600,
+            height:     300,
+            minWidth:   600,
+            minHeight:  300,
+            left:       0,
+            top:        0
         },
         error: {
             frame: "none",
-            width: 600,
-            height: 300,
-            minWidth: 600,
-            minHeight: 300,
-            left: 0,
-            top: 0
+            width:      600,
+            height:     300,
+            minWidth:   600,
+            minHeight:  300,
+            left:       0,
+            top:        0
         }
     },
 
@@ -786,6 +786,17 @@ var TransedEditor = {
             $(".dropup").first().popover('show');
 
         });
+
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-36918929-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = 'https://ssl.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
 
         return self;
     }
